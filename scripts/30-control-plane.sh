924ad62b7c521035a76a77a1fcf0e2df0990b0bb
Eduardo Rolim <ens.rolim@gmail.com>
2022-06-16 10:55:59 -0300

    Correção da inicialização do cluster com um hack de rotas

--- scripts/30-control-plane.sh
+++ scripts/30-control-plane.sh
@@ -6,11 +6,16 @@ echo "####################################################################"
 
 IP=`ip addr show enp0s8 | grep 'inet ' | cut -d/ -f1 | awk '{ print $2}'`
 
-HOST=`hostname -s`.cluster
+HOST=`hostname -s`
 
 echo "$IP $HOST" | sudo tee -a /etc/hosts | sudo tee /tmp/k8s/hosts-entry
 
-sudo kubeadm init --control-plane-endpoint=$HOST:6443 --apiserver-advertise-address=$IP --apiserver-cert-extra-sans=$IP --pod-network-cidr=172.17.0.0/16 --service-cidr=172.16.0.0/16 --node-name=$HOST
+sudo kubeadm init --control-plane-endpoint=$HOST:6443 \
+                  --apiserver-advertise-address=$IP \
+                  --apiserver-cert-extra-sans=$IP \
+                  --pod-network-cidr=172.17.0.0/16 \
+                  --service-cidr=172.16.0.0/16 \
+                  --node-name=$HOST
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
--- scripts/40-worker-node.sh
+++ scripts/40-worker-node.sh
@@ -4,7 +4,20 @@ echo "####################################################"
 echo "############### Join do Worker node ################"
 echo "####################################################"
 
+IP=`ip addr show enp0s8 | grep 'inet ' | cut -d/ -f1 | awk '{ print $2}'`
+
+HOST=`hostname -s`
+
+echo "$IP $HOST" | sudo tee -a /etc/hosts | sudo tee /tmp/k8s/hosts-entry
+
 cat /tmp/k8s/hosts-entry | sudo tee -a /etc/hosts
 chmod 0755 /tmp/k8s/control-plane-join.sh
+
+# Hack para fazer o comando `kubeadm token create` funcionar usando a interface de rede
+# No entanto, esse hack é ineficiente, no que deverá ser alterado para usar um arquivo de
+# config, conforme https://kubernetes.io/docs/reference/config-api/kubeadm-config.v1beta3/
+sudo ip route add default via 192.168.56.1
 sudo /tmp/k8s/control-plane-join.sh
-kubectl label node `hostname -s` node-role.kubernetes.io/worker=worker --kubeconfig /tmp/k8s/cluster-admin.conf
+sudo ip route del default via 192.168.56.1
+
+kubectl label node $HOST node-role.kubernetes.io/worker=worker --kubeconfig /tmp/k8s/cluster-admin.conf
